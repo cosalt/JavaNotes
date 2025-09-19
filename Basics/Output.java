cbcd658d6c35fd18734d96b6c4e514704c482216
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2025-09-19 11:35:21 +0100

    Create Output.java

---
+++ Basics/Output.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // this is my first java program
+        /*
+
+        yo gurt, this is a multi-line comment
+
+         */
+        System.out.print("I like Fried Rice!\n");
+        System.out.print("It's really good\n");
+        System.out.print("Make me Fried Rice");
+        System.out.println("to automatically type this, type sout + tab");
+    }
+}
